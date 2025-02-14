f7f2c1bb6ab554f5a7b3cd49cbd6295b50388f01
Whee30 <99293401+Whee30@users.noreply.github.com>
2025-02-13 22:13:13 -0700

    modified:   local_version.json 	modified:   version_checker.py

--- version_checker.py
+++ version_checker.py
@@ -5,6 +5,7 @@ headers = {
     'Cache-Control': 'no-cache'
 }
 
+# Placeholder dictionary
 remote_data = {}
 
 # Dictionary to store the different files to be updated
@@ -14,13 +15,14 @@ remote_files = {
     "template": "https://raw.githubusercontent.com/Whee30/program_updater/refs/heads/main/template.txt"
 }
 
+# Declare the local filepaths for potential update
 local_files = {
     "program": "program.txt",
     "verbiage": "verbiage.txt",
     "template": "template.txt"
 }
 
-# Load local versions
+# Load local version numbers
 with open('local_version.json', 'r') as local_version_file:
     local_version_data = json.load(local_version_file)
 
@@ -31,46 +33,25 @@ version_response = requests.get(version_url, headers=headers)
 
 def compare_versions():
     if local_version_data['program'] < remote_data['program']:
-        download_update(remote_files['program'], local_files['program'])
+        download_update('program', remote_data['program'])
     if local_version_data['verbiage'] < remote_data['verbiage']:
-        download_update(remote_files['verbiage'], local_files['verbiage'])
+        download_update('verbiage', remote_data['verbiage'])
     if local_version_data['template'] < remote_data['template']:
-        download_update(remote_files['template'], local_files['template'])
+        download_update('template', remote_data['template'])
+    print('Everything is up to date!')
 
-def download_update(remote_version, local_version):
-    print(remote_version)
-    file_response = requests.get(remote_version, headers=headers)
-    with open(local_version, 'wb') as file:
+def download_update(target, version):
+    print(target)
+    file_response = requests.get(remote_files[target], headers=headers)
+    with open(local_files[target], 'wb') as file:
         file.write(file_response.content)
-    print(f"{local_version} was updated.")
+    local_version_data[target] = version
+    with open('local_version.json', 'w') as file:
+        json.dump(local_version_data, file, indent=4)
+    print(f"{target} was updated.")
 
 if version_response.status_code == 200:
     remote_data = version_response.json()
     compare_versions()
 else:
     print(f"Failed to fetch data: {version_response.status_code}")
-
-'''
-def update_local(new_version):
-    with open(p_dest, 'wb') as file:
-        #file.write(p_response.content)
-    with open('local_version.json', 'r') as local_v:
-        local_d = json.load(local_v)
-        local_d['v'] = new_version
-    with open('local_version.json', 'w') as local_v_w:
-        json.dump(local_d, local_v_w, indent=4)
-
-
-
-print(f"Remote version is: {remote_data['v']}, Local version is: {local_data['v']}.")
-
-if remote_data['v'] > local_data['v']:
-    print("remote is bigger")
-    update_local(remote_data['v'])
-elif remote_data['v'] < local_data['v']:
-    print("Remote is smaller")
-elif remote_data['v'] == local_data['v']:
-    print("They're the same")
-else:
-    print("something else happened")
-'''
